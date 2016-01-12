613056c3e4dabf59760a882836d1f4d34b626e50
Danny Tunon <danieltunon@gmail.com>
2016-01-12 18:24:02 -0500

    Modify tweet formatting

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
   var index = streams.home.length - 1;
   while (index >= 0) {
     var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    var $tweet = $('<div class="tweet well"></div>');
+    $tweet.html('<a href="#">@' + tweet.user + '</a>:<p>' + tweet.message + '</p>');
     $tweet.appendTo($stream);
     index -= 1;
   }
