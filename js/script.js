02f9f8118032bbab270bdabfc7433f03e6ee2a0f
Danny Tunon <danieltunon@gmail.com>
2016-01-12 15:28:07 -0500

    Refactor /ready/ to account for changes to html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+$(document).ready(function () {
+  var $stream = $('article.stream');
+  //$stream.html('<h2>Your stream</h2>');
 
   var index = streams.home.length - 1;
-  while(index >= 0){
+  while (index >= 0) {
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+    $tweet.appendTo($stream);
     index -= 1;
   }
 
