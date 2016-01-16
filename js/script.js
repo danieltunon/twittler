d2edba040c90cac87cfc7172f540b1d0877770ee
Danny Tunon <danieltunon@gmail.com>
2016-01-15 20:37:24 -0500

    Do some cleanup on files
    Adds sublime project files to .gitignore and cleans up
    the script.js file (removing trailing spaces and other
    delinting)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /*global moment*/
+
 // Begin /tweetFormatter/ utility function
 // Purpose: produces the properly formatted html for each tweet
 // Arguments:
@@ -6,7 +7,7 @@
 //   * message - the actual message text
 //   * time - the timestamp when the tweet was created
 // Returns: nothing
-// Actions: 
+// Actions:
 //   * formats time into readable form
 //   * inserts arguments into proper place in template HTML
 //
@@ -27,7 +28,7 @@ function tweetFormatter(user, message, time) {
 //   * end - the current final tweet in streams.home
 //   * target - the DOM element where tweets are added
 // Returns: nothing
-// Actions: 
+// Actions:
 //   * iterates through array of tweets if new tweets have been
 //     created but not displayed
 //   * creates a new <div> element for each new tweet
@@ -37,12 +38,13 @@ function tweetFormatter(user, message, time) {
 //     tweets are displayed at the top of the stream
 //
 function displayTweets(start, end, target) {
-  for ( var i = start; i < end; i++ ) {
+  var i = start;
+  for ( i; i < end; i++ ) {
     var tweet = streams.home[i];
-    var $tweet = $('<div class="tweet well" data-timestamp="'
-      + tweet.created_at.toISOString() +'"></div>');
-    $tweet.html( 
-      tweetFormatter(tweet.user, tweet.message, tweet.created_at) 
+    var $tweet = $('<div class="tweet well" data-timestamp="' +
+      tweet.created_at.toISOString() +'"></div>');
+    $tweet.html(
+      tweetFormatter(tweet.user, tweet.message, tweet.created_at)
     );
     $tweet.hide().prependTo(target).fadeIn("slow");
   }
@@ -55,20 +57,20 @@ $(document).ready(function () {
 
   var tweetsDisplayed = 0;
   var totalTweets = streams.home.length;
-  
+
   // call /displayTweets/ on page ready to initially populate stream
   displayTweets(tweetsDisplayed, totalTweets, $stream);
   tweetsDisplayed = totalTweets;
-  
+
   // create new interval timer to update stream with new tweets
-  setInterval(function updateStream() {
+  var updateStream = setInterval( function () {
     totalTweets = streams.home.length;
     displayTweets(tweetsDisplayed, totalTweets, $stream);
     tweetsDisplayed = totalTweets;
   }, 1000);
-  
-  // create new interval to update timestamp
-  setInterval( function updateTimeStamp () {
+
+  // create new interval timer to update timestamp
+  var updateTimeStamp = setInterval( function () {
     $( ".timestamp" ).each( function () {
       var time = $( this ).parents( ".tweet" ).data( "timestamp" );
       $( this ).text( moment( time ).fromNow() );
